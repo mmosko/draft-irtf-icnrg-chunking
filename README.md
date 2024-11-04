7485d98bbc9f165a412426481509714a31e543ce
Marc Mosko <marc@mosko.org>
2024-11-04 12:19:13 -0800

    Move chunking out of ccnx-protocol-rfcs and updates for IETF 121.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # draft-irtf-icnrg-chunking
 CCNX Chunking Protocol
+
+- `docs/` contains presentations or other related materials.
+- `src/` has the RFC materials.
+
+The make process uses the IETF web service to render XML to text, html, and pdf.
---
+++ src/render.py
@@ -0,0 +1,95 @@
+# Copyright 2024 Marc Mosko
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# curl -F "file=@draft-irtf-icnrg-flic-03.xml" -X POST https://author-tools.ietf.org/api/render/text
+
+import requests
+import sys
+import getopt
+
+
+def usage():
+    print('usage: render -i input_file -o output_file -f (text | html)')
+    print()
+
+
+def get_args():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'i:o:f:')
+    except getopt.GetoptError:
+        usage()
+        sys.exit(-2)
+
+    input_file_name = None
+    output_file_name = None
+    output_format_name = None
+
+    for opt, arg in opts:
+        if opt == "-i":
+            input_file_name = arg
+        elif opt == "-o":
+            output_file_name = arg
+        elif opt == "-f":
+            output_format_name = arg
+        else:
+            usage()
+            sys.exit(-3)
+
+    assert input_file_name is not None
+    assert output_file_name is not None
+    assert output_format_name is not None
+
+    return input_file_name, output_file_name, output_format_name
+
+
+def print_logs(output):
+    try:
+        logs = output['logs']
+        errors = logs['errors']
+        warnings = logs['warnings']
+        if len(errors) > 0:
+            print(f'Errors:\n')
+            for x in errors:
+                print(f'\t{x}')
+
+        if len(warnings) > 0:
+            print(f'Warnings:\n')
+            for x in warnings:
+                print(f'\t{x}')
+
+    except KeyError as e:
+        print(f'Output has no key: {e}')
+
+
+if __name__ == "__main__":
+
+    input_file, output_file, output_format = get_args()
+
+    api_text = f'https://author-tools.ietf.org/api/render/{output_format}'
+    with requests.session() as sess:
+        files = {'file': open(input_file, 'rb')}
+        response = sess.post(api_text, files=files)
+        if not response.ok:
+            print(f"status = {response.status_code}, reason = {response.reason}")
+            exit(-1)
+        output = response.json()
+        print_logs(output)
+
+        try:
+            download_url = output['url']
+            print(f'Downloading {download_url} as {output_file}')
+            rendered = sess.get(download_url)
+            with open(output_file,'w') as fh:
+                fh.write(rendered.text)
+        except KeyError:
+            pass
